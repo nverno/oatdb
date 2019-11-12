5c5467a453b81a64a934bb33b389c06e890e1fb7
nverno <noah.v.peart@gmail.com>
2019-11-11 23:56:30 -0500

    add parser to dbinfo.rb -- not worth parsing types after conversion to ll ir

--- dbinfo.rb
+++ dbinfo.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+require "pry"
 
 class Id
   @@count = 6                                         # 1st 6 used in DATA
-  def initialize
-    @@count += 1
-    @n = @@count
+  def initialize(n=nil)
+    @@count += 1 unless n
+    @n = n ? n : @@count
   end
   def to_s; "!#@n" end
 end
@@ -27,12 +28,12 @@ class FunType < DBInfo
 end
 
 class Fun < DBInfo
-  def initialize(line, name)
+  def initialize(name, line)
     super()
-    @line, @name = line, name
+    @name, @line = name, line
   end
   def to_s
-    "#{@id} = distinct !DISubprogram(name: \"#{@name}\", line #{@line}, " +
+    "#{@id} = distinct !DISubprogram(name: \"#{@name}\", line: #{@line}, " +
       "scope: !1, file: !1, isLocal: false, isDefinition: true, " +
       "scopeLine: #{@line}, isOptimized: false, " +
       "flags: DIFlagPrototyped, unit: !0, variables: !2)"
@@ -41,19 +42,20 @@ end
 
 class TBasic < DBInfo
   KINDS = {
-    :I64 => "name: \"int\", size 64, encoding: DW_ATE_signed",
-    :I1 => "name: \"bool\", size 1, encoding: DW_ATE_boolean",
-    :I8 => "name: \"char\", size 8, encoding: DW_ATE_signed_char"
+    :i64 => "name: \"int\", size: 64, encoding: DW_ATE_signed",
+    :i1 => "name: \"bool\", size: 1, encoding: DW_ATE_boolean",
+    :i8 => "name: \"char\", size: 8, encoding: DW_ATE_signed_char"
   }
   def initialize(kind)
     super()
-    @desc = KINDS[kind]
+    @desc = KINDS.has_key?(kind) ? KINDS[kind] : KINDS[:i8]
   end
   def to_s; "#{@id} = !DIBasicType(#{@desc})" end
 end
 
 class TDerived < DBInfo
   def initialize(base, tag=:DW_TAG_pointer_type, size=64)
+    super()
     @base, @tag, @size = base, tag, size
   end
   def to_s
@@ -63,35 +65,93 @@ end
 
 class Var < DBInfo
   def initialize(name, type, line, scope, arg=1)
+    super()
     @name, @type, @line, @scope, @arg = name, type, line, scope, arg
   end
   def to_s
     "#{@id} = !DILocalVariable(name: \"#{@name}\", type: #{@type}, " +
-      "scope: #{@scope}, file: !1, line: #{@line}, arg: #{@arg})"
+      "scope: #{@scope}, file: !1, line: #{@line})" # , arg: #{@arg})"
   end
 end
 
-# call void @llvm.dbg.addr(metadata i32* %4, metadata !14, metadata !DIExpression()), !dbg !15
-class DbgCall < DBInfo
-  def initialize(var, meta_id, call=:declare)
-    @var, @meta_id = var, meta_id
+class Loc < DBInfo
+  def initialize(line, scope, col=1)
+    super()
+    @line, @scope, @col = line, scope, col
   end
   def to_s
-    "call void @llvm.dbg.addr(metadata i32* %4, metadata !14, metadata !DIExpression()), !dbg !15"
-
+    "#{@id} = !DILocation(line: #{@line}, column: #{@col}, scope: #{@scope})"
   end
 end
 
+# Parse lines from .ll file
+class LLParser
+  attr_accessor :dbinfo
+  def initialize(line)
+    @line = line
+    @scope = Id.new(1)                            # file scope
+    @dbinfo = []
+    @types = {}
+  end
 
-ncalls = 0
-ARGF.each_with_index do |line, idx|
-  
-  print ARGF.filename, ":", idx, ";", line
+  def do_call(var, type, meta_id, loc_id, call=:declare)
+    puts "  call void @llvm.dbg.#{call}(metadata #{type} #{var}, " +
+         "metadata #{meta_id}, metadata !DIExpression()), !dbg #{loc_id}"
+  end
+
+  def type_id str
+    return @types[str].id if @types.has_key?(str)
+    t = str[-1] == '*' ? TDerived.new(type_id(str[0...-1])) : TBasic.new(str.to_sym)
+    @types[str] = t
+    return t.id
+  end
+
+  def parse(input)
+    @line += 1
+    input.chomp!
+    
+    if input.empty?
+      puts input
+    elsif input =~ /^\s*[;}]/                     # do nothing lines
+      puts input
+    elsif m = input.match(/define\s+(.+)\s+@([[:alnum:]_]+)(\([^\)]+\))/)
+      f = Fun.new(m[2], @line)
+      @dbinfo << f
+      @scope = f.id
+      puts "#{m[0]} #0 !dbg #{f.id} {"
+    elsif m = input.match(/\s*([%_[:alnum:]]+)\s*=\s*alloca\s+(.+)\s*/)
+      l = Loc.new(@line, @scope)
+      t = type_id(m[2])
+      v = Var.new(m[1].sub("%", ""), t, @line, @scope)
+      @dbinfo << l << v
+      @line += 1                                  # extra line for new call
+      # puts "#{m[0]}, !dbg #{l.id}"
+      puts input
+      do_call(m[1], m[2] + "*", v.id, l.id)
+    elsif input =~ /^\S/                          # non-body lines
+      puts input
+    else
+      l = Loc.new(@line, @scope)
+      @dbinfo << l
+      puts "#{input}, !dbg #{l.id}"
+    end
+  end
+
+  def print_dbinfo
+    @dbinfo.each {|elem| puts elem }
+    @types.values.each { |key| puts key }
+  end
 end
+
+puts "source_filename = \"#{ARGF.filename}\""
+p = LLParser.new(0)
+ARGF.each { |line| p.parse(line) }
 
 # Print debugging information
 DDecls = DATA.read
 printf DDecls, ARGF.filename, __dir__
+p.print_dbinfo
+
 
 
 __END__
